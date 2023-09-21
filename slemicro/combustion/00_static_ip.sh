8125b3961beccdcf40d84ba8b47de8acfbe31f5a
Steven <steven.hardy@suse.com>
2023-09-21 18:29:54 +0300

    fixup

--- slemicro/combustion/00_static_ip.sh
+++ slemicro/combustion/00_static_ip.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Adapted from https://github.com/openSUSE/combustion#perform-modifications-in-the-initrd-environment
-nm_config() {
+if [ ! -z "${VM_STATIC_IP}" ]; then
+	# The NM configuration isn't activated so we have to bring the NIC up manually
+	# otherwise subsequent steps e.g registration will fail
+	ip addr add ${VM_STATIC_IP}/24 dev eth0
+	ip route add default via ${VM_GATEWAY_IP}
+	echo "nameserver ${VM_GATEWAY_IP}" > /etc/resolv.conf
+
 	umask 077 # Required for NM config
 	mkdir -p /etc/NetworkManager/system-connections/
-	cat >/etc/NetworkManager/system-connections/static.nmconnection <<-EOF
+	cat >/etc/NetworkManager/system-connections/eth0.nmconnection <<-EOF
 [connection]
-id=static
+id=eth0
 type=ethernet
+interface-name=eth0
 autoconnect=true
 
 [ipv4]
 method=manual
 dns=${VM_GATEWAY_IP}
 address1=${VM_STATIC_IP}/24,${VM_GATEWAY_IP}
+may-fail=false
 EOF
-}
-
-if [ ! -z "${VM_STATIC_IP}" ]; then
-	if [ "${1-}" = "--prepare" ]; then
-		nm_config # Configure NM in the initrd
-		exit 0
-	fi
-	nm_config # Configure NM in the system
 fi
