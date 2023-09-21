13fe52f6874a74ed9a37607413f4217a3f93d994
Steven <steven.hardy@suse.com>
2023-09-21 15:20:53 +0300

    static-ip WIP

---
+++ slemicro/combustion/00_static_ip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# combustion: network prepare
+set -euxo pipefail
+
+# Adapted from https://github.com/openSUSE/combustion#perform-modifications-in-the-initrd-environment
+nm_config() {
+	umask 077 # Required for NM config
+	mkdir -p /etc/NetworkManager/system-connections/
+	cat >/etc/NetworkManager/system-connections/static.nmconnection <<-EOF
+[connection]
+id=static
+type=ethernet
+autoconnect=true
+
+[ipv4]
+method=manual
+dns=${VM_GATEWAY_IP}
+address1=${VM_STATIC_IP}/24,${VM_GATEWAY_IP}
+EOF
+}
+
+if [ ! -z "${VM_STATIC_IP}" ]; then
+	if [ "${1-}" = "--prepare" ]; then
+		nm_config # Configure NM in the initrd
+		exit 0
+	fi
+	nm_config # Configure NM in the system
+fi
